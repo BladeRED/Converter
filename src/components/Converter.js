4f5041ede335273f86ec29ddd8f8021bf39d7be8
BladeRED <juleray@msn.com>
2022-09-07 16:04:45 +0200

    prototype du convertisseur ok

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,30 +1,33 @@
-import Button from 'react-bootstrap/Button';
+//import Button from 'react-bootstrap/Button'; //
 import '../styles/Converter.css'
+import { useState } from 'react'
 
 
 function Converter() { 
 
-  let number = 84;
+
   let patate = 0.2;
+
+  let [nbPotatoes, setInputNbPotatoes] = useState("");
+  let [nbMeters, setInputNbMeters] = useState("");
   
 
-  let convert = (patate * number).toFixed(2)
+  let convertPotatoes = (patate * nbPotatoes).toFixed(2)
+  let convertMeters = (patate * nbMeters).toFixed(2)
     
     return (
-        <div className="container my-5">
+        <div className="container my-5 text-center">
           <h3 className="text-center"> Conversion de patates en mètre</h3>
           
-         <p className="text-center">Choississez le nombre de patates à convertir:</p>
-         <form className="text-center " method="post">
 
-          <input type="number" className="numberConvert text-center" min="1" step="1"></input>
-          <Button variant="success" className="ms-5">Convertir</Button>{''}
+          <p className="text-center">Patates en mètres :</p>
+          <input type="number" className="numberConvert text-center" min="1" step="1" onChange={(event) => setInputNbPotatoes(event.target.value)}></input>
+          <p> Il faut {nbPotatoes} patates pour faire {convertPotatoes} m</p>
 
-          {patate} {convert}
-
-          <p> Il faut {number} patates pour faire {convert} mètre</p>
-          
-         </form>
+          <p className="text-center">Mètres en patates :</p>
+          <input type="number" className="numberConvert text-center" min="1" step="1" onChange={(event) => setInputNbMeters(event.target.value)}></input>
+          <p> {nbMeters} patates représente une distance d'environ {convertMeters} m</p>
+         
         </div>
       );
 
