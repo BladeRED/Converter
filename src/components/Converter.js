4f65bf9572e942e3fda6856dfa5440ae73fef0b8
BladeRED <juleray@msn.com>
2022-09-08 11:20:13 +0200

    Convertisseur patate metre ok, l'inverse non

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -3,8 +3,8 @@ import '../styles/Converter.css'
 import { useState } from 'react'
 
 
-function Converter() { 
 
+function Converter() { 
 
   let patate = 0.2;
 
@@ -21,11 +21,11 @@ function Converter() {
           
 
           <p className="text-center">Patates en mètres :</p>
-          <input type="number" className="numberConvert text-center" min="1" step="1" onChange={(event) => setInputNbPotatoes(event.target.value)}></input>
+          <input type="number" className="numberConvert text-center" min="1" step="1" onChange={(event) => setInputNbPotatoes(event.target.value)} value={nbPotatoes}></input>
           <p> Il faut {nbPotatoes} patates pour faire {convertPotatoes} m</p>
 
           <p className="text-center">Mètres en patates :</p>
-          <input type="number" className="numberConvert text-center" min="1" step="1" onChange={(event) => setInputNbMeters(event.target.value)}></input>
+          <input type="number" className="numberConvert text-center" min="1" step="1" onChange={(event) => setInputNbMeters(event.target.value)} value={convertPotatoes} ></input>
           <p> {nbMeters} patates représente une distance d'environ {convertMeters} m</p>
          
         </div>
